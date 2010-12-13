c92107f3a4ed4c2f6e79a1550df72f5c8c51bf92
Mook <mook.moz+git@gmail.com>
2010-12-12 21:35:43 -0800

    re-implementation inside blist.xul

--- content/unibrow.js
+++ content/unibrow.js
@@ -5,6 +5,7 @@
 
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://unibrow/unibrow.jsm");
 
 var Unibrow = {
   __proto__: window,
@@ -20,46 +21,23 @@ var Unibrow = {
   browser_addConversation: function Unibrow_browser_addConversation(aConv) {
     // we _always_ want to use the same window for everything.
     var conv = Unibrow.browser._addConversation(aConv);
-    var item = document.createElement("richlistitem");
-    Unibrow.list.appendChild(item);
-    item.setAttribute("linkedpanel", conv.tab.linkedPanel);
-    item.setAttribute("label", conv.tab.label);
+    var contact = Services.Unibrow.Tag.addConversation(aConv);
+    Unibrow.buddyWin.buddyList.observe(contact, "fake-buddy", null);
     return conv;
   },
 
-  /* */
-  openSingletonWindow: function Unibrow_openSingletonWindow(aWindowType) {
-    var URLS = {
-      accounts: "chrome://instantbird/content/accounts.xul",
-      addons: "about:addons",
-      errors: "chrome://global/content/console.xul"
-    };
-
-    var type = aWindowType.replace(/./, String.toUpperCase);
-    var tab = document.getElementById("unibrow" + type + "Tab");
-    if (tab) {
-      Unibrow.list.selectedItem = tab;
+  browser_beginRemoveTab: function Unibrow_browser_beginRemoveTab(
+    aTab, aTabWillBeMoved, aCloseWindowFastpath)
+  {
+    // note that |this| is the tabbrowser
+    // force aCloseWindowFastpath to be false and see what we get
+    var result = this.__proto__._beginRemoveTab.call(
+                   this, aTab, aTabWillBeMoved, false);
+    if (result && result[1]) {
+      // will close window
+      return Unibrow.conversations.reload();
     }
-    else {
-      var frame = document.createElement("browser");
-      frame.setAttribute("type", "chrome");
-      frame.setAttribute("disablehistory", true);
-      frame.setAttribute("id", "unibrow" + type);
-      Unibrow.deck.appendChild(frame);
-      tab = document.createElement("richlistitem");
-      tab.setAttribute("id", "unibrow" + type + "Tab");
-      tab.setAttribute("for", "unibrow" + type);
-      try {
-        let key = "unibrow.tabs." + aWindowType + ".label";
-        tab.setAttribute("label", Unibrow.strings.GetStringFromName(key));
-      }
-      catch (e) {
-        tab.setAttribute("label", type);
-      }
-      Unibrow.list.insertBefore(tab, document.getElementById("unibrowSpacerTab"));
-      Unibrow.list.selectedItem = tab;
-      frame.loadURI(URLS[aWindowType], null, null);
-    }
+    return result;
   },
 
   /* internal methods */
@@ -68,15 +46,11 @@ var Unibrow = {
       // ignore sub-document loads
       return;
     }
+    addEventListener("unload", Unibrow.onUnload, false);
 
     Unibrow.browser.addConversation = Unibrow.browser_addConversation;
+    Unibrow.browser._beginRemoveTab = Unibrow.browser_beginRemoveTab;
     Unibrow.browser.tabContainer.addEventListener("TabSelect", Unibrow.onTabSelect, false);
-    Unibrow.browser.tabContainer.addEventListener("TabClose", Unibrow.onTabClose, false);
-    Unibrow.list.addEventListener("select", Unibrow.onListSelect, false);
-    document.getElementById("unibrowBuddiesTab").label =
-      Unibrow.strings.GetStringFromName("unibrow.tabs.buddies.label");
-    Unibrow.openSingletonWindow("buddies");
-    addEventListener("unload", Unibrow.onUnload, false);
   },
 
   onUnload: function Unibrow_onUnload(aEvent) {
@@ -91,6 +65,7 @@ var Unibrow = {
   },
 
   onTabSelect: function Unibrow_onTabSelect(aEvent) {
+    return;
     var tab = aEvent.originalTarget;
     for (let i = 0; i < Unibrow.list.childNodes.length; ++i) {
       let item = Unibrow.list.childNodes.item(i);
@@ -100,36 +75,14 @@ var Unibrow = {
       }
     }
   },
-
-  onTabClose: function Unibrow_onTabClose(aEvent) {
-    var tab = aEvent.originalTarget;
-    for (let i = 0; i < Unibrow.list.childNodes.length; ++i) {
-      let item = Unibrow.list.childNodes.item(i);
-      if (item.panelId == tab.linkedPanel) {
-        Unibrow.list.removeChild(item);
-        return;
-      }
-    }
-  },
-
-  onListSelect: function Unibrow_onListSelect(aEvent) {
-    var item = Unibrow.list.selectedItem;
-    if (item.hasAttribute("for")) {
-      var panel = document.getElementById(item.getAttribute("for"));
-      Unibrow.deck.selectedPanel = panel;
-    }
-    else if ("tab" in item) {
-      // this is a proxy item
-      Unibrow.deck.selectedPanel = Unibrow.conversations;
-      Unibrow.browserTabs.selectedItem = item.tab;
-      return;
-    }
+  observe: function Unibrow_observe(aSubject, aTopic, aData) {
+    
   }
 };
 
 XPCOMUtils.defineLazyGetter(Unibrow,
                             "conversations",
-                            function()document.getElementById("unibrowConversations"));
+                            function()document.getElementById("conversationFrame"));
 XPCOMUtils.defineLazyGetter(Unibrow,
                             "browser",
                             function()Unibrow.conversations.contentWindow.getBrowser());
@@ -137,11 +90,11 @@ XPCOMUtils.defineLazyGetter(Unibrow,
                             "browserTabs",
                             function()Unibrow.browser.tabContainer);
 XPCOMUtils.defineLazyGetter(Unibrow,
-                            "list",
-                            function()document.getElementById("unibrowList"));
+                            "buddyFrame",
+                            function()document.getElementById("buddyFrame"));
 XPCOMUtils.defineLazyGetter(Unibrow,
-                            "deck",
-                            function()document.getElementById("unibrowDeck"));
+                            "buddyWin",
+                            function()Unibrow.buddyFrame.contentWindow);
 XPCOMUtils.defineLazyGetter(Unibrow,
                             "strings",
                             function()Services.strings.createBundle("chrome://unibrow/locale/unibrow.properties"));
--- jsmodules/unibrow.jsm
+++ jsmodules/unibrow.jsm
@@ -8,7 +8,170 @@ const EXPORTED_SYMBOLS = [];
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource:///modules/imServices.jsm");
 
+function UnibrowContact(aConv) {
+  Cu.reportError(aConv);
+  this._conv = aConv;
+  this._observers = [];
+  this.id = -(Date.now() >>> 0);
+  this.alias = aConv.title;
+}
+
+UnibrowContact.prototype = {
+  __proto__: UnibrowContact.prototype,
+  _conv: null, // conversation
+  _observers: [],
+
+  /** public interface */
+  get conversation() this._conv,
+
+  /** imIContact */
+  id: -1,
+  alias: "",
+  getTags: function UnibrowContact_getTags(aCount) {
+    if (aCount) {
+      aCount.value = 1;
+    }
+    return [UnibrowTag];
+  },
+  move: function UnibrowContact_move(aOldTag, aNewTag) {
+    throw Cr.NS_ERROR_NOT_IMPLEMENTED;
+  },
+  get preferredBuddy() this,
+  getBuddies: function UnibrowContact_getBuddies(aCount) {
+    if (aCount) {
+      aCount.value = 0;
+    }
+    return [];
+  },
+  remove: function UnibrowContact_remove() {
+    throw Cr.NS_ERROR_NOT_IMPLEMENTED;
+  },
+
+  addObserver: function UnibrowTag_addObserver(aObserver) {
+    this.removeObserver(aObserver);
+    this._observers.push(aObserver);
+  },
+  removeObserver: function UnibrowTag_removeObserver(aObserver) {
+    this._observers = this._observers.filter(function(o) o != aObserver);
+  },
+  notifyObservers: function UnibrowTag_notifyObservers(aSubject, aTopic, aData) {
+    this._observers.forEach(function (aObserver) {
+      try {
+        aObserver.observe(aSubject, aTopic, aData);
+      }
+      catch (e) {
+        // silently ignore
+      }
+    });
+  },
+  
+  /** imIStatusInfo */
+  get displayName() this._conv.name,
+  get buddyIconFilename() null,
+  get statusType() Ci.imIStatusInfo.STATUS_AVAILABLE,
+  get online() true,
+  get available() true,
+  get idle() false,
+  get mobile() false,
+  get statusText() "(not implementeed)",
+  get availabilityDetails() 0,
+  get canSendMessage() true,
+  get getTooltipInfo() ({hasMoreElements:function()false, getNext:function() null}),
+  createConversation: function UnibrowContact_createConversation() this._conv,
+
+  /** imIBuddy */
+  // id is reused from imIContact
+  get protocol() {
+    Cu.reportError(this._conv.account.protocol);
+    return this._conv.account.protocol;
+  },
+  get userName() this._conv.name,
+  get normalizedName() this._conv.normalizedName,
+  get contact() this,
+  get preferredAccountBuddy() {
+    if (this._conv instanceof Ci.purpleIConvIM) {
+      return this._conv.buddy;
+    }
+    return null;
+  },
+  getAccountBuddies: function(aCount) {
+    if (aCount) aCount.value = 0;
+    return [];
+  },
+  remove: function() {
+    throw Cr.NS_ERROR_NOT_IMPLEMENTED;
+  },
+  // addObserver is reused from imIContact
+  // removeObserver is reused from imIContact
+  // notifyObservers is reused from imIContact
+  observe: function(aSubject, aTopic, aData) {
+    Cu.reportError("fake contact observed " + aTopic);
+  },
+
+  /** nsISupports */
+  QueryInterface: XPCOMUtils.generateQI([Ci.imIContact,
+                                         Ci.imIStatusInfo,
+                                         Ci.imIBuddy])
+};
+
+
+var UnibrowTag = {
+  _contacts: [],
+  _observers: [],
+
+  /** public interface */
+  addConversation: function UnibrowTag_addConverstation(aConv) {
+    var contacts = this._contacts.filter(function(c) c.conversation == aConv);
+    if (contacts.length > 0) {
+      return contacts[0];
+    }
+    var contact = new UnibrowContact(aConv);
+    this._contacts.push(contact);
+    this.notifyObservers(contact, "contact-added", contact.id);
+    return contact;
+  },
+  removeConversation: function UnibrowTag_removeConversation(aConv) {
+    var removed = [];
+    this._contacts = this._contacts.filter(function (c) {
+      return (c.conversation == aConv) && removed.push(c);
+    });
+    return removed;
+  },
+
+  /** imITag */
+  id: -(Date.now() >>> 0),
+  name: Services.strings
+                .createBundle("chrome://unibrow/locale/unibrow.properties")
+                .GetStringFromName("unibrow.groups"),
+  getContacts: function UnibrowTag_getContacts(aCount) {
+    if (aCount) {
+      aCount.value = this._contacts.length;
+    }
+    return Array.slice(this._contacts);
+  },
+  addObserver: function UnibrowTag_addObserver(aObserver) {
+    this.removeObserver(aObserver);
+    this._observers.push(aObserver);
+  },
+  removeObserver: function UnibrowTag_removeObserver(aObserver) {
+    this._observers = this._observers.filter(function(o) o != aObserver);
+  },
+  notifyObservers: function UnibrowTag_notifyObservers(aSubject, aTopic, aData) {
+    this._observers.forEach(function (aObserver) {
+      try {
+        aObserver.observe(aSubject, aTopic, aData);
+      }
+      catch (e) {
+        // silently ignore
+      }
+    });
+  },
+  /** nsISupports */
+  QueryInterface: XPCOMUtils.generateQI([Ci.imITag])
+};
+
 Services.Unibrow = {
+  Tag: UnibrowTag
 };
 
 XPCOMUtils.defineLazyGetter(Services.Unibrow,
